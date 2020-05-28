a66bf618ae18e3ea868547cd96a5effd762f9669
Damian Kryński <krynskid@student.mini.pw.edu.pl>
2020-05-28 15:52:24 +0200

    Basic class

---
+++ SudokuMultiplayerBackend/SudokuGameEngine/Board.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuGameEngine
+{
+    class Board
+    {
+        public Field[,] Field = new Field[9, 9];
+
+        // metody: 
+        // sprawdzenie czy wygrana
+        // wstawienie numeru w pole - usuniecie przez wstawienie -1/null 
+        // sprawdzanie czy pole jest poprawnie uzupełnione/uzupełniane 
+    }
+}
---
+++ SudokuMultiplayerBackend/SudokuGameEngine/Field.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SudokuGameEngine
+{
+    public class Field
+    {
+        public int number;
+
+        public bool isBlocked;
+    }
+}
