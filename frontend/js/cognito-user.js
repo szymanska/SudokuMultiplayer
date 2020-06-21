d9859d58244a796c0866f194e3cd258460573f12
Ania Szymanska <aejszymanska@gmail.com>
2020-06-21 17:13:51 +0200

    signout

---
+++ frontend/js/cognito-user.js
@@ -0,0 +1,11 @@
+let poolData = {
+    UserPoolId: _config.cognito.userPoolId,
+    ClientId: _config.cognito.userPoolClientId
+};
+
+let userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+function getCognitoUsername(){
+    cognitoUser = userPool.getCurrentUser()
+    return cognitoUser.username
+}
